9a1eb8aaf46ae5f3403c9fe4a89718225e086471
KDONJS <80475432+KDONJS@users.noreply.github.com>
2022-05-08 06:57:23 -0500

    yorlin cambios 1.0.8

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -13,17 +13,16 @@ class RegisterController extends Controller
 
     public function store(Request $request)
     {
-        //dd($request);
-        //dd($request->get('user-name'));
-
         //validación
 
         $this->validate($request,[
             'name'=>'required|max:30',
-            'unser_name'=>'required|unique:user|min:3|max:20',
-            'email'=>'required|unique:user|email|max:60',
-            'password'=>'required'
+            'unsername'=>'required|unique:users|min:3|max:20',
+            'email'=>'required|unique:users|email|max:60',
+            'password'=>'required|confirmed|min:8'
         ]);
+
+        dd($request);
 
     }
 
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -32,9 +32,9 @@
           </div>
 
           <div>
-            <label for="user-name" class=" text-gray-500 m-2 block">Username</label>
-            <input id="user-name" name="user-name" type="text"   class="appearance-none relative block w-full px-3 py-2 border  placeholder-gray-500 text-gray-900  rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm @error('user-name') border-red-500 @enderror" placeholder="Usuario">
-            @error('user-name')
+            <label for="username" class=" text-gray-500 m-2 block">Username</label>
+            <input id="username" name="username" type="text"   class="appearance-none relative block w-full px-3 py-2 border  placeholder-gray-500 text-gray-900  rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm @error('username') border-red-500 @enderror" value="{{ old('username') }}" placeholder="Usuario">
+            @error('username')
             <br>
               <div class="p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800" role="alert">
                   <span class="font-medium">Alerta!</span> {{$message}}
@@ -43,7 +43,7 @@
           </div>
           <div>
             <label for="email" class=" text-gray-500 m-2 block">Email address</label>
-            <input id="email" name="email" type="email" autocomplete="email"  class="appearance-none relative block w-full px-3 py-2 border placeholder-gray-500 text-gray-900  rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm @error('email') border-red-500 @enderror" placeholder="Email de registro">
+            <input id="email" name="email" type="email" autocomplete="email"  class="appearance-none relative block w-full px-3 py-2 border placeholder-gray-500 text-gray-900  rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm @error('email') border-red-500 @enderror" value="{{ old('email') }}" placeholder="Email de registro">
 
             @error('email')
             <br>
@@ -54,7 +54,7 @@
           </div>
           <div>
             <label for="password" class="text-gray-500 m-2 block">Password</label>
-            <input id="password" name="password" type="password"  class="appearance-none relative block w-full px-3 py-2 border placeholder-gray-500 text-gray-900  rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm @error('password') border-red-500 @enderror" placeholder="Password de registro">
+            <input id="password" name="password" type="password"  class="appearance-none relative block w-full px-3 py-2 border placeholder-gray-500 text-gray-900  rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm @error('password') border-red-500 @enderror"  placeholder="Password de registro">
             @error('password')
             <br>
               <div class="p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800" role="alert">
